d216d3a04722152510e7ed189bceb4eb8ab05259
Shawn Allen <shawn.allen@gsa.gov>
2016-08-08 14:32:53 -0700

    make it more robust

--- index.js
+++ index.js
@@ -4,23 +4,44 @@ const MARKER = '---\n'
 
 module.exports = function(options) {
   var buffer = ''
-  return through(function(chunk, enc, next) {
+  var log = (options && options.debug === true)
+    ? function() { console.warn.apply(console, arguments) }
+    : function() { }
+
+  return through(function read(chunk, enc, next) {
     if (typeof buffer === 'string') {
       buffer += chunk.toString()
+      var tmp
+
       if (buffer.indexOf(MARKER) === 0) {
+
         var lastIndex = buffer.lastIndexOf(MARKER)
         if (lastIndex > 0) {
           var frontmatter = buffer.substr(MARKER.length, lastIndex - MARKER.length)
-          // console.warn('parsing:', JSON.stringify(frontmatter))
-          frontmatter = yaml.safeLoad(frontmatter, options)
+          try {
+            frontmatter = yaml.safeLoad(frontmatter, options)
+          } catch (error) {
+            return this.emit('error', error), next()
+          }
           this.emit('frontmatter', frontmatter)
           buffer = buffer.substr(lastIndex + MARKER.length)
-          // console.warn('buffered:', JSON.stringify(buffer))
-          return next(null, buffer)
+          return next(null, buffer), (buffer = null)
+        } else {
+          // log('buffering:', JSON.stringify(buffer))
+          return next()
         }
+
+      } else if (buffer.length > MARKER.length) {
+        return next(null, buffer), (buffer = null)
       }
-    } else {
-      return next(null, chunk)
     }
+    return next(null, chunk)
+  }, function end(done) {
+    if (typeof buffer === 'string') {
+      var error = new Error('Unclosed front matter')
+      error.buffer = buffer
+      this.emit('error', error)
+    }
+    done()
   })
 }
